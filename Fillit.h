ade6c4cbfeaebc8b6cbf6b7533b6db2e804b58a8
Samuel KLEPPER <sklepper@e2r2p2.42.fr>
2018-04-16 17:16:06 +0200

    minor changes

--- Fillit.h
+++ Fillit.h
@@ -1,4 +1,24 @@
 #ifndef FILLIT_FILLIT_H
 # define FILLIT_FILLIT_H
+# define BUFF_SIZE 546
+
+#include "Libft/libft.h"
+
+typedef struct	s_point
+{
+	int x;
+	int y;
+}				t_point;
+
+typedef struct	s_tetrominos
+{
+	t_point					hash1;
+	t_point					hash2;
+	t_point					hash3;
+	t_point					hash4;
+	int						length;
+	int						width;
+	struct s_tetrominos		*next;
+}				t_list;
 
 #endif
